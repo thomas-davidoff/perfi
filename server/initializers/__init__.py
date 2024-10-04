2bb788b456ef4f49e98474b2e08bce8c69757fda
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-10-04 18:52:58 -0400

    Add custom app logger.

--- server/initializers/__init__.py
+++ server/initializers/__init__.py
@@ -1,7 +1,12 @@
 from .database import db
 from .migrate import migrate
+from .app_logger import create_logger
+
+
+app_logger = create_logger("APP_LOGGER")
 
 
 def init_app(app):
+    app_logger.debug("Initializing application.")
     db.init_app(app)
     migrate.init_app(app, db)
---
+++ server/initializers/app_logger.py
@@ -0,0 +1,42 @@
+# Make sure to look into rotating log files, so one logger can be defined but use custom destinations.
+#
+# # logger_setup.py
+
+import logging
+from colorama import Fore, Style
+import os
+
+
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    # TODO: Fix pathname to filename, but from root of project, not system.
+    format = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(pathname)s:%(lineno)d)"
+    )
+
+    FORMATS = {
+        logging.DEBUG: Fore.CYAN + format + Style.RESET_ALL,
+        logging.INFO: Fore.GREEN + format + Style.RESET_ALL,
+        logging.WARNING: Fore.YELLOW + format + Style.RESET_ALL,
+        logging.ERROR: Fore.RED + format + Style.RESET_ALL,
+        logging.CRITICAL: Fore.RED + Style.BRIGHT + format + Style.RESET_ALL,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def create_logger(logger_name: str):
+    # set log level in .env.{env} file.
+    log_level = os.getenv("LOG_LEVEL", "DEBUG").upper()
+    # Configure loggers
+    logging.basicConfig(level=log_level)
+
+    # Apply custom formatter
+    for handler in logging.root.handlers:
+        handler.setFormatter(CustomFormatter())
+
+    return logging.getLogger(logger_name)
