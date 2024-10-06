bddf1e1f99b642f3ddcb8775447f5e58699451c0
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-10-06 12:04:59 -0400

    Add timestamp mixin to user model.

--- server/database/models/user.py
+++ server/database/models/user.py
@@ -1,15 +1,21 @@
 # database/models/user.py
 
-from initializers import db, bcrypt
-from flask_login import UserMixin
+from extensions import db, bcrypt
 from config import logger
-from sqlalchemy import Text
-
+from sqlalchemy import Text, func, text
 from initializers import bcrypt
 
 
+class TimestampMixin(object):
+    # NOW() AT TIME ZONE \'UTC\' is postgresql specific.
+    # However, it ensures ts will be created in utc time without having to call python datetime
+    created_at = db.Column(
+        db.DateTime(timezone=True), default=text("NOW() AT TIME ZONE 'UTC'")
+    )
+
+
 # Just a basic table for now to test things.
-class User(UserMixin, db.Model):
+class User(TimestampMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -22,7 +28,6 @@ class User(UserMixin, db.Model):
 
     @property
     def password(self):
-        logger.debug("password prop")
         raise AttributeError("Password is write-only.")
 
     @password.setter
