f266f9e3125f77cbf265b7c5b9c0a901bfedec9c
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-10-10 22:16:22 -0400

    Fix Timestamp mixin to make created_at read-only.

--- server/database/models/timestamp_mixin.py
+++ server/database/models/timestamp_mixin.py
@@ -3,9 +3,16 @@ from sqlalchemy import func
 
 
 class TimestampMixin(object):
-    # NOW() AT TIME ZONE \'UTC\' is postgresql specific.
-    # However, it ensures ts will be created in utc time without having to call python datetime
-    # created_at = db.Column(
-    #     db.DateTime(timezone=True), default=text("NOW() AT TIME ZONE 'UTC'")
-    # )
-    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    created_at = db.Column(
+        db.DateTime(timezone=True), default=func.now(), nullable=False
+    )
+
+    @property
+    def created_at(self):
+        """Allow reading the created_at value."""
+        return self._created_at
+
+    @created_at.setter
+    def created_at(self, value):
+        """Disallow setting created_at after initial creation."""
+        raise AttributeError("created_at is read-only.")
