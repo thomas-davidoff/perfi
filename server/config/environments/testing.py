e64592968a328b83b744116904806956b4a64b6f
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-10-12 11:27:24 -0400

    Use docker for testing.

--- server/config/environments/testing.py
+++ server/config/environments/testing.py
@@ -10,4 +10,11 @@ class TestingConfig(DefaultConfig):
         self.DEBUG = True
         self.TESTING = True
 
-        self.SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+        # self.SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+        # self.SQLALCHEMY_DATABASE_URI = (
+        #     "postgresql+psycopg2://poopy:12345@127.0.0.1:8888/tests"
+        # )
+
+        self.SQLALCHEMY_DATABASE_URI = (
+            "postgresql://postgres:mysecretpassword@localhost:5432/testdb"
+        )
--- server/conftest.py
+++ server/conftest.py
@@ -6,8 +6,54 @@ import os
 from initializers import load_env, load_configuration, get_logger
 
 from tests.helpers import TransactionFactory
+import pytest
+from pytest_postgresql import factories
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from extensions import db
+from initializers import load_env, load_configuration, get_logger
+import psycopg
+import time
+import docker
+
 
 environment = "testing"
+DEFAULT_DATABASE_NAME = "postgres"
+
+DB_CONFIG = {
+    "dbname": "testdb",
+    "user": "postgres",
+    "password": "mysecretpassword",
+    "host": "localhost",
+    "port": 5432,
+}
+
+CONNECTION = f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}"
+
+
+def is_postgres_available(db_name=None):
+    db_config = DB_CONFIG.copy()
+    db_config["dbname"] = db_name or DEFAULT_DATABASE_NAME
+    try:
+        with psycopg.connect(**db_config) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT 1")
+            return True
+    except psycopg.OperationalError:
+        raise
+        return False
+
+
+postgresql_in_docker = factories.postgresql_noproc(
+    host=DB_CONFIG["host"],
+    port=DB_CONFIG["port"],
+    password=DB_CONFIG["password"],
+)
+
+postgresql = factories.postgresql(
+    "postgresql_in_docker",
+    dbname=DB_CONFIG["dbname"],
+)
 
 
 def pytest_configure():
@@ -25,7 +71,7 @@ def config():
 
 
 @pytest.fixture
-def app(config):
+def app(config, session):
 
     from app import create_app
 
@@ -33,16 +79,68 @@ def app(config):
 
     app = create_app(config, init_logger)
 
+    # Update app config with test database connection
+    app.config.update({"SQLALCHEMY_DATABASE_URI": CONNECTION})
+
     with app.app_context():
         db.create_all()
         yield app
-        db.session.remove()
+
+        session.rollback()
         db.drop_all()
 
 
 @pytest.fixture()
 def client(app) -> FlaskClient:
     return app.test_client()
+
+
+@pytest.fixture()
+def session(postgresql_connection):
+    engine = create_engine(CONNECTION)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    yield session
+
+    session.close()
+
+
+@pytest.fixture(scope="session")
+def postgresql_container():
+    client = docker.from_env()
+    container = client.containers.run(
+        image="postgres:12",
+        auto_remove=True,
+        name="test-postgres",
+        ports={f"5432/tcp": DB_CONFIG["port"]},
+        environment={"POSTGRES_PASSWORD": DB_CONFIG["password"]},
+        detach=True,
+        remove=True,
+    )
+
+    timeout = 10
+
+    # Wait for the container to start
+    start = time.time()
+    while container.status != "running":
+        container.reload()
+        elapsed = time.time() - start
+        if elapsed > timeout:
+            raise TimeoutError("Postgres container did not start in time")
+
+    # Wait for the database to be ready
+    start = time.time()
+    while not is_postgres_available():
+        elapsed = time.time() - start
+        if elapsed > timeout:
+            raise TimeoutError("Postgres database did not start in time")
+
+    try:
+        yield container
+    finally:
+        container.stop()
 
 
 @pytest.fixture()
@@ -53,3 +151,8 @@ def valid_user():
 @pytest.fixture()
 def transaction_factory():
     return TransactionFactory()
+
+
+@pytest.fixture()
+def postgresql_connection(postgresql_container, postgresql):
+    yield postgresql
