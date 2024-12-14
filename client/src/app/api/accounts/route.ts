630cb9654bb64a3f6c71f602e6e836d15e5a2120
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-12-14 12:46:22 -0500

    Move authenticated fetch wrapper into app/api.

--- client/src/app/api/accounts/route.ts
+++ client/src/app/api/accounts/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { authFetch } from '@/lib/authFetch'
+import { authFetch } from '@/app/api/authFetch'
 
 export async function GET() {
     const session = await getServerSession(authOptions);
