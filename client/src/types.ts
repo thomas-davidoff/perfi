fee577d70b356c20ed94e1ae5999fdc05ab3b5b4
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-12-14 11:59:41 -0500

    Add types for user and account.

--- client/src/types.ts
+++ client/src/types.ts
@@ -15,3 +15,21 @@ export interface Transaction {
     merchant: string,
     updated_at: string
 }
+
+export interface Account {
+    account_type: string,
+    balance: number,
+    created_at: string,
+    id: string,
+    name: string,
+    updated_at: string,
+    user_id: User
+}
+
+export interface User {
+    id: string,
+    updated_at: string,
+    username: string,
+    created_at: string,
+    email: string
+}
