c6d32b048037618da6c42b3642802d3146609d01
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-12-15 13:07:30 -0500

    Add action column.

--- client/src/app/dashboard/transactions-table/columns.tsx
+++ client/src/app/dashboard/transactions-table/columns.tsx
@@ -1,6 +1,15 @@
 import { Checkbox } from "@/components/ui/checkbox"
 import { Transaction } from "@/types"
 import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export const columns: ColumnDef<Transaction>[] = [
     {
@@ -42,5 +51,37 @@ export const columns: ColumnDef<Transaction>[] = [
     {
         accessorKey: "account.name",
         header: "Account",
-    }
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+          const transaction = row.original
+
+          return (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <p>Some icon: todo</p>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem
+                  onClick={() => navigator.clipboard.writeText(transaction.id)}
+                >
+                  Copy transaction ID
+                </DropdownMenuItem>
+                {/* todo: view details popover */}
+                <DropdownMenuItem>View details</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                {/* todo: update handler */}
+                <DropdownMenuItem>Update transaction</DropdownMenuItem>
+                {/* todo: delete handler */}
+                <DropdownMenuItem>Delete transaction</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )
+        },
+      },
 ]
