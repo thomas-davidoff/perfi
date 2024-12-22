8c30453087c70df24053880a2b1eb79ff2df7bd4
Thomas Davidoff <thomas.davidoff@gmail.com>
2024-12-22 13:00:21 -0500

    Add transaction details to details sheet.

--- client/src/app/transactions/transactions-table/transctions-details-sheet.tsx
+++ client/src/app/transactions/transactions-table/transctions-details-sheet.tsx
@@ -3,30 +3,39 @@
 import React from "react";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetDescription, SheetFooter, SheetClose } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
+import { Transaction } from "@/types";
 
-export function TransactionDetailsSheet({ isOpen, onClose }: { isOpen: boolean, onClose: () => void }) {
+
+const Row = ({ name, val }: { name: string, val: string }) => {
+    return (
+        <>
+            <div className="col-span-1">
+                <b>{name}</b>
+            </div>
+            <div className="col-span-3">
+                {val}
+            </div>
+        </>
+    )
+}
+
+export function TransactionDetailsSheet({ isOpen, onClose, transaction }: { isOpen: boolean, onClose: () => void, transaction: Transaction }) {
     return (
         <Sheet open={isOpen} onOpenChange={(open) => !open && onClose()}>
             <SheetContent side="right">
                 <SheetHeader>
                     <SheetTitle>Transaction Details</SheetTitle>
                     <SheetDescription>
-                        View or edit transaction details.
+                        id: {transaction.id}
                     </SheetDescription>
                 </SheetHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <label htmlFor="name" className="text-right">
-                            Name
-                        </label>
-                        <input id="name" defaultValue="Transaction Name" className="input" />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <label htmlFor="amount" className="text-right">
-                            Amount
-                        </label>
-                        <input id="amount" defaultValue="$100" className="input" />
-                    </div>
+                <div className="grid gap-4 py-2 grid-cols-4">
+                    <Row name="Date" val={transaction.date} />
+                    <Row name="Merchant" val={transaction.merchant} />
+                    <Row name="Amount" val={transaction.amount.toString()} />
+                    <Row name="Category" val={transaction.category} />
+                    <Row name="Description" val={transaction.description} />
+                    <Row name="Account" val={transaction.account.name} />
                 </div>
                 <SheetFooter>
                     {/* This button will trigger the close */}
