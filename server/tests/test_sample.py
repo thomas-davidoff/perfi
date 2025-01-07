d027cb2d38c33c86335539df53b5137c82823db2
Thomas Davidoff <thomas.davidoff@gmail.com>
2025-01-06 23:48:46 -0500

    Move test factories into dedicated subpackage.

--- server/tests/test_sample.py
+++ server/tests/test_sample.py
@@ -1,6 +1,7 @@
-async def test_create_user(db_session):
-    from tests.helpers.factories import UserFactory
+from tests.factories import UserFactory
 
+
+async def test_create_user(db_session):
     user = await UserFactory.create(db_session, username="testuser")
     assert user.username == "testuser"
     assert user.email is not None
