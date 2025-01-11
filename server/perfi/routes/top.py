20a58f2485b5f9ae18efa6e9a86123e6c8ab748f
Thomas Davidoff <thomas.davidoff@gmail.com>
2025-01-10 22:17:50 -0500

    Add container names to docker-compose and add healthchecks for client/server.

--- server/perfi/routes/top.py
+++ server/perfi/routes/top.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from perfi.schemas.user import UserResponse
 from perfi.core.database import User
 from perfi.core.dependencies.current_user import get_current_user
@@ -12,3 +12,11 @@ async def whoami(current_user: User = Depends(get_current_user)):
     Return details of the current user based on the JWT token.
     """
     return UserResponse.model_validate(current_user)
+
+
+@router.get("/health")
+async def health():
+    """
+    Returns 200 to indicate liveness
+    """
+    return Response(status_code=200)
