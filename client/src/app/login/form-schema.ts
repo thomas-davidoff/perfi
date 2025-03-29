9dba099c009d1100aee08d4b62a86b060d60a916
Thomas Davidoff <thomas.davidoff@gmail.com>
2025-03-29 14:10:10 -0400

    Remove client directory for now.

--- client/src/app/login/form-schema.ts
+++
@@ -1,8 +0,0 @@
-
-import { z } from "zod"
-export default z.object({
-    username: z.string().min(6, {
-        message: "Username must be at least 6 characters."
-    }),
-    password: z.string(),
-});
